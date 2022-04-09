da5a9bb1be7194bf066c0645ad5ee95afcd5611b
olufemi <olukannistephen@gmail.com>
2022-04-09 09:51:59 -0700

    good

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,9 @@
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	return (0);
-}
-{
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");                                                                              
 	return (0);
 }
