a94e700b8712dc5b9fdb7a1564988fc4b8e51dc5
olufemi <olukannistephen@gmail.com>
2022-04-09 17:00:26 -0700

    go

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,12 @@
 /**
  * main - Entry point
  * Description: use printf
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
+
 	return (0);
 }
