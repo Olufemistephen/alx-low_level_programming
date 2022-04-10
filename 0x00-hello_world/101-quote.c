a74798d83fea0682dd90b4e0db249820ad805f24
olufemi <olukannistephen@gmail.com>
2022-04-09 17:36:53 -0700

    o

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,21 +3,11 @@
 /**
  * main - print to string
  * Description - coding i c language
- * Return: 1
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	long 1 = 59;
-	long fd = 1;
-	long syscall = 1;
-	long ret = 0;
-	__asm__ ("syscall"
-	: "=a" (ret)
-	: "a" (syscall),
-	"D" (fd),
-	"S" (s),
-	"d" (l));
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
