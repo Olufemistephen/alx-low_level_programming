b235b8a4cbb13dffc812c20169f69687e71dd8ac
olufemi <olukannistephen@gmail.com>
2022-04-09 17:58:07 -0700

    good

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,8 @@
 #include <time.h>
 
 /**
- * main - entry point
- * Description: prints  if a number is positive or negative.
+ * main - Entry point
+ * Description: prints if a number is positive or negative.
  * Return: 0
  */
 
